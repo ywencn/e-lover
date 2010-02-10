730804951f1c292ca2db9c1db012d5143eeb76fc
ywencn <yangdachuan@yeah.net>
2010-02-10 14:12:53 +0800

    design home layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,39 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def title(page_title, show_title = true)
+    @content_for_title = page_title.to_s
+    @show_title = show_title
+  end
+
+  def show_title?
+    @show_title
+  end
+  
+  def stylesheet(*args)
+    content_for(:head) { stylesheet_link_tag(*args.map(&:to_s)) }
+  end
+
+  def javascript(*args)
+    args = args.map { |arg| arg == :defaults ? arg : arg.to_s }
+    content_for(:head) { javascript_include_tag(*args) }
+  end
+  
+# define my date formate 
+  def my_date(date)
+     Date.today == date.to_date ? date.strftime("%H:%M:%S") : date.strftime("%Y-%m-%d")
+  end
+# define my date formate
+  def standard_date(date)
+     date.strftime("%Y-%m-%d %H:%M:%S")
+  end    
+  
+  def error_messages_for(object_name, options = {})
+    options = options.symbolize_keys
+    object = instance_variable_get("@#{object_name}")
+    unless object.errors.empty?
+      error_lis = []
+      object.errors.each{ |key, msg| error_lis << content_tag("li", msg) }
+      content_tag("div", content_tag(options[:header_tag] || "h2", "发生#{object.errors.count}个错误" ) + content_tag("ul", error_lis), "id" => options[:id] || "errorExplanation", "class" => options[:class] || "errorExplanation" )
+    end
+  end
 end
